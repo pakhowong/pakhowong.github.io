d08372670f6946c3776276031fdecdf2b60b3b70
Pakho Wong <parcowph@connect.hku.hk>
2026-03-20 04:16:50 +0000

    Create day_links.js

---
+++ day_links.js
@@ -0,0 +1,26 @@
+const DAY_LINKS = {
+  "2025-04-18": {
+    url: "https://drive.google.com/file/d/1MSc6fa7umFZ4CQBIRWSQmAMYFmSmuhDr/view?usp=drivesdk",
+    name: "LGW to GYD"
+  },
+  "2025-05-12": {
+    url: "https://drive.google.com/file/d/1EpIe8Ay8_-6G5DdWAb6x28tiwZgldBOu/view?usp=drivesdk",
+    name: "NRT to LGW"
+  },
+  "2026-03-26": {
+    url: "https://drive.google.com/file/d/1vqktjU51xKapXwjX5VNEIhCLtn5DFyiL/view?usp=drivesdk",
+    name: "LGW to ARN"
+  },
+  "2026-04-12": {
+    url: "https://drive.google.com/file/d/1vqktjU51xKapXwjX5VNEIhCLtn5DFyiL/view?usp=drivesdk",
+    name: "TIA to GLA"
+  },
+  "2026-05-03": {
+    url: "https://drive.google.com/file/d/1Gz5onLtwm5Pj3t9PlvZfFl7wZ-njSK4t/view?usp=drivesdk",
+    name: "LHR to SFO"
+  },
+  "2026-05-26": {
+    url: "https://drive.google.com/file/d/1t-x6Ac3Eqt3CNcBJxVktLZDOMekVE70V/view?usp=drivesdk",
+    name: "MIA to LHR"
+  }
+};
