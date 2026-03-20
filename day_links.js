d5c99f5b02568b29892765684188e6f07340a359
Pakho Wong <parcowph@connect.hku.hk>
2026-03-20 16:35:35 +0000

    Update day_links.js

--- day_links.js
+++ day_links.js
@@ -1,4 +1,40 @@
 const DAY_LINKS = {
+  "2024-10-18": {
+    url: "",
+    name: " to "
+  },
+  "2024-10-23": {
+    url: "",
+    name: " to "
+  },
+  "2024-10-27": {
+    url: "",
+    name: " to "
+  },
+  "2024-11-09": {
+    url: "",
+    name: " to "
+  },
+  "2024-11-19": {
+    url: "",
+    name: " to "
+  },
+  "2025-01-02": {
+    url: "",
+    name: " to "
+  },
+  "2025-01-07": {
+    url: "",
+    name: " to "
+  },
+  "2025-01-20": {
+    url: "",
+    name: " to "
+  },
+  "2025-01-22": {
+    url: "",
+    name: " to "
+  },
   "2025-03-08": {
     url: "https://drive.google.com/file/d/15ZDEaAfNSVTO47Ud89tr2QnOxGoI6qxu/view?usp=drivesdk",
     name: "LHR to HND"
@@ -6,6 +42,14 @@ const DAY_LINKS = {
   "2025-03-16": {
     url: "https://drive.google.com/file/d/1_aO1Z84b8N5A0n3d9pQG4oyGftE1GdIz/view?usp=drivesdk",
     name: "HND to LHR"
+  },
+  "2025-03-22": {
+    url: "",
+    name: " to "
+  },
+  "2025-04-06": {
+    url: "",
+    name: " to "
   },
   "2025-04-18": {
     url: "https://drive.google.com/file/d/1MSc6fa7umFZ4CQBIRWSQmAMYFmSmuhDr/view?usp=drivesdk",
@@ -15,9 +59,61 @@ const DAY_LINKS = {
     url: "https://drive.google.com/file/d/1EpIe8Ay8_-6G5DdWAb6x28tiwZgldBOu/view?usp=drivesdk",
     name: "NRT to LGW"
   },
+  "2025-05-17": {
+    url: "",
+    name: " to "
+  },
+  "2025-06-07": {
+    url: "",
+    name: " to "
+  },
+  "2025-07-02": {
+    url: "",
+    name: " to "
+  },
+  "2025-07-07": {
+    url: "",
+    name: " to "
+  },
+  "2025-07-08": {
+    url: "",
+    name: " to "
+  },
+  "2025-08-09": {
+    url: "",
+    name: " to "
+  },
+  "2025-08-23": {
+    url: "",
+    name: " to "
+  },
   "2025-10-16": {
     url: "https://drive.google.com/file/d/14uIZWvqpsh9Ib-1S6-YnaIMZOw8Xqqjp/view?usp=drivesdk",
     name: "HKG to LHR"
+  },
+  "2025-11-24": {
+    url: "",
+    name: " to "
+  },
+  "2025-12-02": {
+    url: "",
+    name: " to "
+  },
+  "2025-12-25": {
+    url: "",
+    name: " to "
+  },
+  "2026-01-03": {
+    url: "",
+    name: " to "
+  },
+  "2026-02-06": {
+    url: "",
+    name: " to "
+  },
+  "2026-02-20": {
+    url: "",
+    name: " to "
   },
   "2026-03-26": {
     url: "https://drive.google.com/file/d/1vqktjU51xKapXwjX5VNEIhCLtn5DFyiL/view?usp=drivesdk",
